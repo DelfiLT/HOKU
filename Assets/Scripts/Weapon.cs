23e571d66166bf865aa305bf2f48632c6087eae0
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-10-23 16:20:54 -0300

    WeaponSprite

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,9 @@ public class Weapon : MonoBehaviour, Iweapon
     private string bulletName;
     public Transform spawn;
 
+    [SerializeField] private SpriteRenderer weaponSprite;
+    [SerializeField] List<Sprite> weaponSprites;
+
     private PlayerInputs playerInput;
 
     private void Start()
@@ -15,27 +18,6 @@ public class Weapon : MonoBehaviour, Iweapon
         playerInput = new PlayerInputs();
         playerInput.Player.Enable();
         playerInput.Player.Shoot.performed += Shoot;
-    }
-
-    public void PickWeapon(WeaponType weaponType)
-    {
-        switch (weaponType)
-        {
-            case WeaponType.AutoCannon:
-                bulletName = "ACBullet";
-                break;
-            case WeaponType.BigSpace:
-                bulletName = "BSBullet";
-                break;
-            case WeaponType.Rockets:
-                bulletName = "RBullet";
-                break;
-            case WeaponType.Zapper:
-                bulletName = "ZBullet";
-                break;
-            default:
-                break;
-        }
     }
 
     public void SpawnBullet()
@@ -52,5 +34,35 @@ public class Weapon : MonoBehaviour, Iweapon
     public void Shoot(InputAction.CallbackContext context)
     {
         SpawnBullet();
+    }
+
+    public void PickWeapon(WeaponType weaponType)
+    {
+        switch (weaponType)
+        {
+            case WeaponType.AutoCannon:
+                StartCoroutine(LoadWeapon("ACBullet", 0));
+                break;
+            case WeaponType.BigSpace:
+                StartCoroutine(LoadWeapon("BSBullet", 1));
+                break;
+            case WeaponType.Rockets:
+                StartCoroutine(LoadWeapon("RBullet", 2));
+                break;
+            case WeaponType.Zapper:
+                StartCoroutine(LoadWeapon("ZBullet", 3));
+                break;
+            default:
+                break;
+        }
+    }
+
+    IEnumerator LoadWeapon(string name, int weaponNum)
+    {
+        bulletName = name;
+        weaponSprite.sprite = weaponSprites[weaponNum];
+        yield return new WaitForSeconds(60);
+        bulletName = "";
+        weaponSprite.sprite = null;
     }
 }
